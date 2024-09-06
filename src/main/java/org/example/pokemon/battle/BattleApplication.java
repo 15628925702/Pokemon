191dd19e1b7e59a8a00cbaab2e1641638cd3a51d
ONeofSu <148432545+ONeofSu@users.noreply.github.com>
2024-09-06 08:24:16 +0800

    9/6 8:23
    图形化界面基本完成

--- src/main/java/org/example/pokemon/battle/BattleApplication.java
+++ src/main/java/org/example/pokemon/battle/BattleApplication.java
@@ -6,6 +6,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import org.example.pokemon.HelloApplication;
 
@@ -38,12 +39,25 @@ public class BattleApplication extends Application {
         pokemon1.getPokeDataFromDb("皮卡丘");
         pokemon1.setPokeSkill("冲撞",0);
         pokemon1.setPokeSkill("十万伏特",1);
+        pokemon1.setPokeSkill("电击",2);
+
         PokemonData pokemon2 = new PokemonData();
         pokemon2.getPokeDataFromDb("小火龙");
         pokemon2.setPokeSkill("冲撞",0);
         pokemon2.setPokeSkill("喷火",1);
+
+        PokemonData pokemon3 = new PokemonData();
+        pokemon3.getPokeDataFromDb("杰尼龟");
+        pokemon3.setPokeSkill("冲撞",0);
+        pokemon3.setPokeSkill("水枪",1);
+
+        PokemonData pokemon4 = new PokemonData();
+        pokemon4.getPokeDataFromDb("妙蛙种子");
+        pokemon4.setPokeSkill("冲撞",0);
+        pokemon4.setPokeSkill("种子炸弹",1);
+
         Battle battle = new Battle();
-        battle.initBattle(pokemon1,pokemon2,this);
+        battle.initBattle(pokemon4,pokemon1,this);
     }
 
     //改写标签文本
@@ -53,6 +67,11 @@ public class BattleApplication extends Application {
     public void setEffectLabelText(String text) {
         controller.setEffectLabelText(text);
     }
+    public void setReturnButtonText(String text) {
+        controller.returnButton.setText(text);
+        controller.returnButton.setVisible(true);
+        controller.returnButton.setDisable(false);
+    }
     //增加标签文本
     public void addStatusLabelText(String text) {
         controller.addStatusLabelText(text);
@@ -60,5 +79,28 @@ public class BattleApplication extends Application {
     //更新血量状态
     public void updateHpStatus(int role1_cur_hp, int role2_cur_hp,int role1_hp, int role2_hp) {
         controller.setHpStatus(role1_cur_hp,role2_cur_hp,role1_hp,role2_hp);
+    }
+    //获得技能信息
+    public void getSkillInfo(String skill1, String skill2, String skill3,String skill4,String pp1,String pp2,String pp3,String pp4) {
+        controller.getSkillInfo(skill1,skill2,skill3,skill4,pp1,pp2,pp3,pp4);
+    }
+    //显示宠物
+    public void showPet(Image poke1,Image poke2){
+        controller.myImage.setImage(poke1);
+        controller.opImage.setImage(poke2);
+    }
+    //播放受击动画
+    public void showDamageAnimation(int i)  {
+        if(i==1){
+            controller.showDamageAnimation();
+        }else if(i==2){
+            controller.showDamageAnimation();
+        }
+    }
+    //显示通知
+    public void showNotice(String notice){
+        controller.noticePane.setVisible(true);
+        controller.noticeLabel.setText(notice);
+        controller.noticeButton.setDisable(false);
     }
 }
